cdc3550cf66bb81731efdc7f5a112cfbde1adc32
tundelakins <tundelakins4real@yahoo.com>
2023-01-19 00:30:34 +0000

    BACKEND STORAGE

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
-"""
-Implements an expiring web cache and tracker
-"""
+
+'''
+method that implements an expiring web cache and tracker
+'''
+
 from typing import Callable
 from functools import wraps
 import redis
 import requests
-redis_client = redis.Redis()
 
 
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
+def requests_counter(method: Callable) -> Callable:
+    """ Counts how many times a request has been made
+    """
+    r = redis.Redis()
+
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(f'{url}')
+    def wrapper(url):
+        """ wrapper fxn that counts actual no of requests made"""
+        r.incr(f"count:{url}")
+        cached_ = r.get(f"cached:{url}")
         if cached:
             return cached.decode('utf-8')
-        redis_client.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
     return wrapper
 
 
-@url_count
+@requests_counter
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
-
+    """ obtains html content for a given site url and returns it.
+    """
+    resp = requests.get(url)
+    return resp.text
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
-
